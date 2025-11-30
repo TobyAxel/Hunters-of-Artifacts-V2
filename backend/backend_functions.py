0d41cd9f4fe9b88bcb1b93f78b4e169ac4ca94aa
aleksvis <aleksi.visuri@outlook.com>
2025-11-30 18:53:08 +0200

    Merge remote-tracking branch 'origin/Aleksi' into Aleksi Backend functoions and app.py Fixing!

--- backend/backend_functions.py
+++ backend/backend_functions.py
@@ -21,19 +21,19 @@ def get_game(game_id):
     return results
 
 
-def get_event(game_id):
+def get_event(game_id, event_list, event):
     cursor.execute("SELECT * FROM game WHERE id = %s", (game_id,))
     rows = cursor.fetchall()
     results = _rows_to_dicts(rows)
-
+    event_list = event_list[0]
     # Check if player is out of moves
     if results[0]['moves'] == 0 and results[0]['event_id'] is None:
         return "Not enough moves to explore more."
 
     # Check if there is an event already going on
     if results[0]['event_id'] is None:
-        event_id = random.randint(0, len(event_list) - 1)
-        cursor.execute("UPDATE game SET event_id = %s, event_state = 0, moves = moves - 1 WHERE id = %s", (event_id, game_id,))
+        event_id = random.randint(0, len(event_list - 1))
+        cursor.execute("UPDATE game SET event_id = %s, event_state = 0, moves = moves - 1 WHERE id = %s", (event_id, game_id),)
         event = event_list[event_id]
         return event.states[0].text
 
@@ -51,10 +51,8 @@ def create_new_game(data):
     [players, config] = [data.get('players'), data.get('config')]
 
     # Create game
-    cursor.execute(
-        "INSERT INTO game (round, max_round, moves, modifier, archived) VALUES (1, %s, 2, %s, false)",
-        (config['max_round'], config['modifier'])
-    )
+    cursor.execute("INSERT INTO game (round, max_round, moves, modifier, archived) VALUES (1, %s, 2, %s, false)",
+    (config['max_round'], config['modifier']))
     new_game_id = cursor.lastrowid
 
     # Create players
@@ -64,7 +62,6 @@ def create_new_game(data):
 
     # Set first player's turn
     cursor.execute('UPDATE game SET player_turn = (SELECT id FROM player WHERE game_id = %s LIMIT 1) WHERE id = %s', (new_game_id, new_game_id))
-
     return new_game_id
 
 def get_players(game_id):
@@ -79,6 +76,7 @@ def get_player(player_id):
     results = _rows_to_dicts(rows)
     return results
 
+def event(game_id, event_in_db, event_list, event_option):
     # Get current event, current event state, event choice
     current_event = event_list[event_in_db[0]['event_id']]
     event_state = current_event.states[event_in_db[0]['event_state']]
