5e83d961eb855763f1e07874794d017f7df18312
MiroVart <vartialamiro6@gmail.com>
2025-12-03 14:04:53 +0200

    Added rare events actually this time

---
+++ backend/events/rare_events.py
@@ -0,0 +1,56 @@
+from backend.game_classes import *
+from backend.events.event_funcs import *
+from backend.items.items import *
+import random
+
+gambler_table_event = Event([
+    State("You find a hidden gambling ring. A dealer invites you to wager 400€. Do you play?\n 1. Yes\n2. No", [
+        Choice(add_item_money, [gamblers_lucky_coin, 30, random.randint(-500, 500)], 2),
+        Choice(None, None, 1),
+    ]),
+    State("You walk away from the shady table.", [
+        Choice(None, None, "final"),
+    ]),
+    State("Press enter to continue", [
+        Choice(None, None, "final"),
+    ])
+])
+
+lost_ticket_event = Event([
+    State("You find a plane ticket lying on a bench. It’s valid for a short-distance flight. Take it?\n1. Yes\n2. No ", [
+        Choice(add_item_money, [extra_ticket, 85, 100], 2),
+        Choice(None, None, 1),
+    ]),
+    State("You leave it. Maybe someone else will find it.", [
+        Choice(None, None, "final"),
+    ]),
+    State("Press enter to continue", [
+        Choice(None, None, "final"),
+    ])
+])
+
+strange_collector_event = Event([
+    State("A Strange collector approaches you. He offers to buy one of your items at a 'good price'.\n1. Yes\n2. No", [
+        Choice(sell_item, [600], 2),
+        Choice(None, None, "final"),
+    ]),
+    State("You decline the collector's offer", [
+        Choice(None, None, "final"),
+    ]),
+    State("Press enter to continue", [
+        Choice(None, None, "final"),
+    ])
+])
+
+abandoned_luggage_event = Event([
+    State("You find some unclaimed luggage at a quiet station. take it?\n1. Yes\n2. No", [
+        Choice(add_item_money, [energy_drink, 20, random.randint(0, 600)], 2),
+        Choice(None, None, 1),
+    ]),
+    State("You decided to leave it be.", [
+        Choice(None, None, "final"),
+    ]),
+    State("Press enter to continue", [
+        Choice(None, None, "final"),
+    ])
+])
