866eb5ef993a69fa7b081fb0bde513a759e4f14c
Akseli Hyvönen <akselihyv@gmail.com>
2025-12-04 16:57:57 +0200

    Modified get event and post event to handle events better, added all items as objects, frontend event management and handling

--- backend/game_classes.py
+++ backend/game_classes.py
@@ -8,7 +8,8 @@ class State:
         self.choices = choices
 
 class Choice:
-    def __init__(self, function, arguments, next_state):
+    def __init__(self, text, function, arguments, next_state):
+        self.text = text
         self.function = function
         self.arguments = arguments or [] # Will break if no list at all given
         self.next_state = next_state
